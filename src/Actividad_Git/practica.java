cf0b89352f82fd9c72d60db061d3145e91ada4c7
marselo-4 <carlosmartinezguixa@gmail.com>
2024-11-11 16:23:23 +0100

    Update practica.java

--- src/Actividad_Git/practica.java
+++ src/Actividad_Git/practica.java
@@ -10,6 +10,14 @@ public class practica {
 		
 		// User 1 fer la suma dels elements de sumArray
 		
+		int suma = 0;
+		for (int vueltas = 0; vueltas < prodArray.length; vueltas++) {
+			
+			suma += sumArray[vueltas];
+			
+		}
+		
+		System.out.println("Total: " + suma + "");
 		
 		
 		// User 2 fer la resta dels elements de subArray
